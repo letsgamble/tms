c1dce27a989a7552fa2f84e35ddb7f3a0a0b9388
Kamil Wesołowski <letsgamble92@gmail.com>
2022-08-19 23:44:34 +0200

    beta version

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import timeit
 import keyboard
 import ezsheets
+from datetime import timedelta, datetime
+import time
 
 
 class TMS:
@@ -10,31 +12,44 @@ class TMS:
         self.calculator = 0
         self.start = 0
         self.stop = 0
+        self.start_date = 0
+        self.check_free_row()
+        self.time_now = datetime.now()
+        self.sh.update(1, 1, 'Start time:')
+        self.sh.update(2, 1, 'End time:')
+        self.row_to_start = 0
+        self.minute_counter = 0
 
     def check_free_row(self):
-        ...
+        self.row_to_start = self.sh.rowCount + 1
+        return
 
     def start_timer(self):
         print(f'This program will track your time')
         print(f'Please press "S" on keyboard to end')
         self.start = timeit.default_timer()
-        return self.start
+        self.start_date = self.time_now
+        return self.sh.update(1, self.row_to_start, str(self.start_date)[0:18])
 
     def stop_timer(self):
+        start_time = time.time()
         while True:
+            if time.time() - start_time >= 60:
+                print('Another minute passed, updating the sheet...')
+                self.minute_counter += 1
+                self.timer_calc()
+                start_time = time.time()
             if keyboard.is_pressed("s"):
                 self.stop = timeit.default_timer()
+                print(f'"S" detected, finishing work.')
                 return self.stop
 
     def timer_calc(self):
-        self.start_timer()
-        self.stop_timer()
-        self.calculator = self.stop - self.start
-        print(self.calculator)
-        return self.sh.update(1, 1, self.calculator)
+        return self.sh.update(2, self.row_to_start, str(datetime.now() + timedelta(minutes=self.minute_counter))[0:18])
 
 
 timer = TMS()
-# print(str(timer.timer_calc())[0:1])
+timer.check_free_row()
+timer.start_timer()
+timer.stop_timer()
 timer.timer_calc()
-# timer.sh.update(1, 1, )
